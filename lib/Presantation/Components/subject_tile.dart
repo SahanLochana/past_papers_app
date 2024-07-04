8d7dd286fb14eaf4bd4b0d37a317c0ce4cae91c4
sahan lochana <w.sahanlochana@gmail.com>
2024-07-04 17:39:06 +0530

    update gitignore

--- lib/Presantation/Components/subject_tile.dart
+++ lib/Presantation/Components/subject_tile.dart
@@ -7,32 +7,37 @@ class SubjectTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Theme.of(context).scaffoldBackgroundColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          const Icon(
-            Icons.airplay_rounded,
-            size: 32,
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Text(
-                subjectModel.subjectName,
-                softWrap: true,
-                textAlign: TextAlign.start,
+    return GestureDetector(
+      onTap: () {
+        // TODO : create navigation and pass the subject id
+      },
+      child: Container(
+        width: 150,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            const Icon(
+              Icons.airplay_rounded,
+              size: 32,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  subjectModel.subjectName,
+                  softWrap: true,
+                  textAlign: TextAlign.start,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
