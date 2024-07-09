4ddd8b68d685fa4733a7970be9c9ddbfdaacb599
sahan lochana <w.sahanlochana@gmail.com>
2024-07-09 20:34:06 +0530

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:past_papers_app/Presantation/Theme/app_theme.dart';
@@ -7,11 +9,23 @@ import 'package:past_papers_app/Routes/route_config.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // init firebase
+  await Firebase.initializeApp();
+
+  // caching enabled
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
+
   // init hive flutter
   await Hive.initFlutter();
 
   // open box for store subject details
   await Hive.openBox("subjectsBox");
+
+  // openr box for store recent files details
   await Hive.openBox("recentBox");
   runApp(const MainApp());
 }
